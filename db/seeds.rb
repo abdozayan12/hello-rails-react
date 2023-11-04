b1340752f6a9ea1c6fad00ddd45fd49172b7c25d
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-04 18:36:42 +0200

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
+# Examples:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+
+    Greeting.create(message: "Welcome to my website!")
+    Greeting.create(message: "Hey there!")
+    Greeting.create(message: "Good day!")
+    Greeting.create(message: "Greetings #1!")
+    Greeting.create(message: "Happy Coding!")
